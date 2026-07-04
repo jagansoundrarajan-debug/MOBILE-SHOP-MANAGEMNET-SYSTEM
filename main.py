7dc7f02262b17b7b4ac403c2bdc08fff35c5669b
jagansoundrarajan-debug <jagansoundrarajan@gmail.com>
2026-07-04 21:24:09 +0530

    Initialize database before launching the app
    Added database initialization before starting the application.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# main.py
-
+#main.py
 import tkinter as tk
 
+from database import initialize_database
 from ui.login_ui import open_login_window
 
-root = tk.Tk()
+# Create database and tables
+initialize_database()
 
+# Start application
+root = tk.Tk()
 root.withdraw()
 
 open_login_window(root)
